5205f00ce02d5a46505b1610cd485e7f2d1f8085
suhanachaudhary212 <suhanasc9149@gmail.com>
2024-08-31 20:44:25 +0530

    ....

--- frontend/zoomclone/src/contexts/AuthContext.jsx
+++ frontend/zoomclone/src/contexts/AuthContext.jsx
@@ -2,12 +2,12 @@ import axios from "axios";
 import httpStatus from "http-status";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import server from '../environment.js';
 
 export const AuthContext = createContext({});
 
 const client = axios.create({
-    baseURL: `http://localhost:8080/api/v1/users`
+    baseURL: `${server}/api/v1/users`
 })
 
 
--- frontend/zoomclone/src/environment.js
+++ frontend/zoomclone/src/environment.js
@@ -1,8 +1,9 @@
-let IS_PROD = true;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+const isProd=true
+const server = isProd?
+    "https://zoom-video-call-app-3.onrender.com" :
 
     "http://localhost:8000"
+  
 
 
 export default server;
